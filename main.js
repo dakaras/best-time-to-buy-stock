172f931423682e937e6b8779d7f63fb545cf369b
dakaras <dan.karagiannis@protonmail.ch>
2020-01-26 23:19:34 -0600

    3rd commit

--- main.js
+++ main.js
@@ -17,3 +17,17 @@
 // Output: 0
 // Explanation: In this
 // case, no transaction is done, i.e.max profit = 0.
+var maxProfit = function (prices) {
+    let buy = prices[0]
+    prices[0] = 0
+    let profit = 0
+    for (let i = 1; i < prices.length; i++) {
+        if (buy > prices[i]) {
+            buy = prices[i]
+            prices[i] = 0
+        } else {
+            profit = Math.max(prices[i] - buy, profit)
+        }
+    }
+    return profit
+};
